78784b748efe876d9da15e50f422ac616f666b34
orifrove <orifsokirhodzaev@gmail.com>
2026-04-15 20:19:29 +0500

    feat: Create table users

--- apps/users/models.py
+++ apps/users/models.py
@@ -2,9 +2,9 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
-    email = models.EmailField(unique=True)
-    phone = models.CharField(max_length=20)
-    passport_number = models.CharField(max_length=20)
+    # email = models.CharField(max_length=50)
+    phone = models.CharField(max_length=10)
+    passport_number = models.CharField(max_length=9)
 
     def __str__(self):
-        return self.email
+        return self.phone
